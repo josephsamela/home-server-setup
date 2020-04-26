f333d7b425defc3e303d668ce1ed845db9cdd91b
Joseph Samela <josephsamela@gmail.com>
2020-04-26 16:37:09 -0400

    Create host-setup.sh

---
+++ host-setup.sh
@@ -0,0 +1,12 @@
+
+# Install `zfs` for creating zfs file systems.
+sudo apt install zfs
+
+# Install `docker` to run containers
+sudo apt install docker docker-compose
+
+# Install `samba` to create smb fileshare
+sudo apt install samba
+
+# Install `cockpit` management software
+sudo apt install cockpit realmd tuned cockpit-packagekit cockpit-docker qemu-kvm install kvmtool cockpit-machines
